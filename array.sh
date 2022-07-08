cf0a7cb419eaa908e2618a68811e4da1303d9b5b
ppavlov28 <ppavlov28@gmail.com>
2022-07-08 12:51:42 +0300

    Update array.sh
    v1

--- array.sh
+++ array.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # Place your code here
+
+folder_num=$1
+
+folder_count=1
+
+for folder_index in {a..z}
+do
+        echo $folder_index
+        if [[ $folder_count -le $folder_num ]]
+        then
+                mkdir ./folder_$folder_index
+                let "folder_count=$folder_count+1"
+        else
+                break
+        fi
+done
