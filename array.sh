e2313716adee2b6bd3a29407b1f8bbd8a5ea5817
ppavlov28 <ppavlov28@gmail.com>
2022-07-10 14:12:35 +0300

    Update array.sh
    v5

--- array.sh
+++ array.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Place your code here
+
 folder_num=$1
+
+folder_count=1
 
 if [[ $folder_num -ge 1 ]] && [[ $folder_num -le 32 ]]
 then
     for folder_index in {a..z}
     do
-        if [[ -d "folder_$folder_index" ]]
-        then
-            echo "Folder folder_$folder_index is exist"
-        else
-            if [[ $folder_count -le $folder_num ]]
+	if ! [[ -d "folder_$folder_index" ]]
+	then
+	    if [[ $folder_count -le $folder_num ]]
             then
-                echo "folder_$folder_index $folder_count"
+	            #echo "folder_$folder_index $folder_count"
                 mkdir folder_$folder_index
                 let "folder_count=$folder_count+1"
             else
                 exit 0
-            fi
         fi
+	fi
     done
 else
     exit 1
 fi
-
