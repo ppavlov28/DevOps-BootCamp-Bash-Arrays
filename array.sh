dc09fdd4de1234209aeb76986db64f9905ea3543
ppavlov28 <ppavlov28@gmail.com>
2022-07-11 16:51:37 +0300

    Add check for folder exist

--- array.sh
+++ array.sh
@@ -8,13 +8,16 @@ if [[ $folder_num -ge 1 ]] && [[ $folder_num -le 32 ]]
 then
     for folder_index in {a..z}
     do
-	if [[ $folder_count -le $folder_num ]]
-            then
-	        sudo mkdir folder_$folder_index
-                let "folder_count=$folder_count+1"
-            else
-                exit 0
-	fi
+        if ! [[ -d "./folder_$folder_index" ]] ; then
+
+            if [[ $folder_count -le $folder_num ]]
+                then
+                    mkdir folder_$folder_index
+                    let "folder_count=$folder_count+1"
+                else
+                    exit 0
+            fi
+        fi
     done
 else
     exit 1
